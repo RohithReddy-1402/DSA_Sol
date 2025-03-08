5a5b03d1b20a3d96ed41a78ec84c914b2ee7f0a9
Rohith_coder <r14v18@gmail.com>
2025-03-08 06:53:33 +0530

    updated

--- POTD/LC/March_8.py
+++ POTD/LC/March_8.py
@@ -2,23 +2,18 @@ class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
         max_count=-1
         temp=0
-        for i in blocks:
-            if i=='B':
+        for i in blocks[:k]:
+            if i=='W':
                 temp+=1
-            else:
-                max_count=max(max_count,temp)
-                temp=0
-        if max_count>=k:
-            return 0
-        else:
-            min_toggle=float('inf')
-            for i in range(len(blocks)-k+1):
-                temp=0
-                for j in range(i,i+k):
-                    if not blocks[j]=='B':
-                        temp+=1
-                min_toggle=min(min_toggle,temp)
-            return min_toggle
+        ans,l=temp,0
+        for i in range(k,len(blocks)):
+            if blocks[l]=='W':
+                temp-=1
+            l+=1
+            if blocks[i]=='W':
+                temp+=1
+            ans=min(ans,temp)
+        return ans
 # TC:(K*(N-k+1))
 # SC:O(1)
 # Approach:loop through the string and find the min of toggles required.

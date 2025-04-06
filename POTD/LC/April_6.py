63e39249b44d51b9ba13a38ff3d115c8214ced46
RohithReddy <r14v18@gmail.com>
2025-04-06 10:12:52 +0530

    April_06

--- POTD/LC/April_6.py
+++ POTD/LC/April_6.py
@@ -1,16 +1,12 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        
         if not nums:
             return []
-
         nums.sort()
         n = len(nums)
         dp = [1] * n
         prev = [-1] * n
-
         max_index = 0
-
         for i in range(n):
             for j in range(i):
                 if nums[i] % nums[j] == 0:
@@ -19,11 +15,12 @@ class Solution:
                         prev[i] = j
             if dp[i] > dp[max_index]:
                 max_index = i
-
         result = []
         i = max_index
         while i >= 0:
             result.append(nums[i])
             i = prev[i]
-
         return result[::-1] 
+# TC: O(n^2)
+# SC: O(n)
+# Approach: Sort the array and use dp by storing the largest and previous index of the largest divisible subset
